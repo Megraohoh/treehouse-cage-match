42abbdc71fefb299e042975550694c5365a39fd9
Meghan Debity <mdebity@gmail.com>
2017-04-20 20:27:04 -0500

    jquery and javascript being added. grabbing player input

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,42 @@
-console.log("hello");
+$(document).ready(function() {
+
+	$(".btn").click(function(){ 
+		let player1String = $("#player1").val();
+		let player2String = $("#player2").val();
+
+		    if(player1String === "") {
+			alert("Which mortal do you wish to sacrifice to the cage?!");
+			}
+			if(player2String === "") {
+			alert("Which mortal do you wish to sacrifice to the cage?!");
+			}
+
+		const player1 = [];
+		const player2 = [];
+
+
+	const loadPlayer1 = () => {
+		return new Promise((resolve, reject) => {
+			$.ajax("https://teamtreehouse.com/" + player1String + ".json")
+			.done((data) => resolve(data))
+			.fail((error) => reject(error));
+		});
+	};
+
+	const loadPlayer2 = () => {
+		return new Promise((resolve, reject) => {
+			$.ajax("https://teamtreehouse.com/" + player2String + ".json")
+			.done((data1) => resolve(data1))
+			.fail((error1) => reject(error1));
+		});
+	};
+
+
+	
+console.log("p1", player1String);
+console.log("p2", player2String);
+
+
+	});
+
+});
